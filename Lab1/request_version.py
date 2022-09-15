da5f767c623997b134e80495dcecf3cc420f6b63
Tomas <tomas.peschke@gmail.com>
2022-09-14 18:32:44 -0600

    Updating python file to return source code of itself

--- Lab1/request_version.py
+++ Lab1/request_version.py
@@ -1,3 +1,4 @@
 import requests
 
-print(requests.__version__)
+res = requests.get("https://raw.githubusercontent.com/Frotaboom/CMPUT-404-Labs/master/Lab1/request_version.py?token=GHSAT0AAAAAABYK5FCVJ7W44JA4NAY553N2YZCOHLQ")
+print(res.text)
